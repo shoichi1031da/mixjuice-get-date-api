dfcc93b5932d83933f3395673772bee1993531dd
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-22 22:54:04 +0900

    fix ver1.0

---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+const nowDate = () => {
+    let date = new Date();
+
+    let year = date.getFullYear() + "\n";
+    let month = date.getMonth() + 1  + "\n";
+    let day = date.getDate() + "\n";
+    let hour = date.getHours() + "\n";
+    let minite = date.getMinutes() + "\n";
+    let second = date.getSeconds() + "\n";
+
+    let now = [year,month,day,hour,minite,second];
+    return now;
+}
+
+app.get("/",(req,res) => {
+    let sendDate = "";
+    let index = 0;
+    if(req.query.i)index = req.query.i;
+    index = parseInt(index,10);
+    let now = nowDate();
+    for(let i=0;i<now.length;i++){
+        sendDate = sendDate + "[" + (index + i) + "]=" + now[i];
+    }
+    console.log(sendDate);
+    res.send(sendDate);
+    }
+)
+
+app.listen(PORT, () => {
+    console.log("runnning server");
+})
